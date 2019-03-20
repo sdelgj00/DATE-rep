2dfdd8ce6b1aa7e43c54bd229633cb6beba6f570
Saul Delgado Jiemno <sdelgj00@estudiantes.unileon.es>
2019-03-20 14:22:34 +0100

    Ejercicios sueltos

--- src/es/unileon/prg/tema7/Date.java
+++ src/es/unileon/prg/tema7/Date.java
@@ -31,7 +31,7 @@ public class Date{
 		}
 		return salida;
 	}
-	public String SeasonOfMonth(){
+	public String nameOfMonth(){
 		String Smonth="";
 		switch(_month){
 			case 1:
@@ -73,8 +73,27 @@ public class Date{
 		}
 		return Smonth;
 	}
+	public String seasonOfDate(){
+		String season="_";
+		if(_month==1||_month==2||_month==12){
+			season="Invierno";
+		}
+		if(_month==3||_month==4||_month==5){
+			season="Primavera";
+		}
+		if(_month==6||_month==7||_month==8){
+			season="Verano";
+		}
+		if(_month==9||_month==10||_month==11){
+			season="Invierno";
+		}
+		return season;
+	
+
+	}
 	public String toString() {
 		return this._day + "/" + this._month + "/" + this._year;
+		
 	}
 	public int getDay(){
 		return _day;
@@ -106,30 +125,57 @@ public class Date{
 	}
 	public boolean isSameYear(Date date){
 		boolean result=false;
-		if(_year=date.getYear()){
+		if(_year==date.getYear()){
 			result=true;
 		}
 		return result;
 	}
 	public boolean isSameMonth(Date date){
 		boolean result=false;
-		if(_month=date.getMonth()){
+		if(_month==date.getMonth()){
 			result=true;
 		}
 		return result;
 	}
 	public boolean isSameDay(Date date){
 		boolean result=false;
-		if(_day=date.getDay()){
+		if(_day==date.getDay()){
 			result=true;
 		}
 		return result;
 	}
 	public boolean isSame(Date date){
-		if(this.i
+		boolean result=false;
+		if(this.isSameDay(date)&&this.isSameMonth(date)&&this.isSameYear(date)){
+			result=true;
+		}
+		return result;
 	}
-	
-	
-	
+	public String mesesQueFaltan()throws DateException{
+		StringBuilder meses= new StringBuilder();
+		for(int i=_month; i<=12; i++){
+			setMonth(i);
+			meses.append(nameOfMonth()+" ");
+		}
+		return meses.toString(); 
+	}
+	public Date randomDate();
+		int day, month, year;
+		month=Math.random()*12 +1;
+		if(_month==1||_month==3||_month==5||_month==7||_month==8||month==10||month==12){
+			day=Math.random()*31+1;
+		}else
+		
+		if(isDayOfMonth(day, month)){
+			_day=day;
+			_month=month;
+			_year=year;		
+		}else{
+			throw new DateException("Fecha inválida: "+day+"/"+month+"/"+year);
+		
 }
+	
+	
+	}
+
 
--- src/es/unileon/prg/tema7/MainDate.java
+++ src/es/unileon/prg/tema7/MainDate.java
@@ -4,13 +4,20 @@ public class MainDate {
 
 	public static void main(String[] args) {
 
-		Date today;
+		Date today, xday;
 
 		try {
-			today = new Date(5, 13, 2017);
+			today = new Date(5, 8, 2017);
+			xday = new Date(5, 8, 2017);
 			System.out.println(today.toString());
+			System.out.println("Mes:" +today.nameOfMonth());
+			System.out.println("Estación:" +today.seasonOfDate());
+			System.out.println("IGUALDAD:" +today.isSame(xday));
+			System.out.println("Meses hasta final de año: "+today.mesesQueFaltan());
+			
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
+
 		}
 
 	}
